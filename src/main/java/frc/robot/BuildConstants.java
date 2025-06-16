7a5df2fa0e4bc3737a3a2226e986f89953586025
FRC4926 <programmingteam@team4926.org>
2025-06-16 19:47:53 -0400

    Initial setup (need USB stick)

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "ReefscapeMain";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "4abbf4f9a8a79fe2547f2a1a9c5be2d664aafac6";
+  public static final String GIT_DATE = "2025-06-16 18:51:09 EDT";
+  public static final String GIT_BRANCH = "lafayette";
+  public static final String BUILD_DATE = "2025-06-16 19:20:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1750116024693L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -24,7 +30,7 @@ import frc.robot.Constants.ReefscapeState;
 import frc.robot.reefscape.Reefscape;
 import frc.robot.subsystems.ThroughboreEncoder;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
@@ -51,6 +57,20 @@ public class Robot extends TimedRobot {
     // Encoder enc2 = new Encoder(new DigitalInput(0), new DigitalInput(1));
 
     public Robot() {
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("ReefscapeMain", "AdvantageLogging"); // Set a metadata value
+
+        if (isReal()) {
+            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+            Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+        } else {
+            setUseTiming(false); // Run as fast as possible
+            String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+            Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+            Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+        }
+
+        Logger.start();
 
         timer.start();
         timer2.start();
